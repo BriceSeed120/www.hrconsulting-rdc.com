d3d0038c1a7b0d05070a3f6aca61de3b8a8d281f
MD SUJAN PRODHAN <etbdsoft@gmail.com>
2021-01-23 20:42:33 +0600

    remove add new button fro order page

--- resources/views/dashboard/orders/index.blade.php
+++ resources/views/dashboard/orders/index.blade.php
@@ -72,12 +72,6 @@
         <div
             class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
             <h1 class="h2">{{ $title }}</h1>
-            <div class="btn-toolbar mb-2 mb-md-0">
-                <a href="{{ route('menus.create') }}" type="button" class="btn btn-sm btn-outline-secondary">
-                    <span data-feather="plus"></span>
-                    Add new Page Menu
-                </a>
-            </div>
         </div>
 
 
