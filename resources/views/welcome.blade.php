509fc514f897eb148946bf8ad57607206697a7b0
unknown <Brice Nde>
2022-03-23 17:58:43 +0100

    Web site name update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content=" One of the best Software Company . We are providing a best software solution for your business.">
-        <meta name="keywords" content="Best Software company- seed120 Ltd ">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
 
-       <title>Seed120 Ltd -Software Solutions Company</title>
+       <title>HR Consulting RDC</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
